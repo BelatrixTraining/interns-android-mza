891bfe50004d9a0f0be960df9c486847a418f707
eselman <evaselman@gmail.com>
2018-12-11 00:59:40 -0300

    Adding import for log

--- helloworld/app/src/main/java/com/training/interns/mza/helloworld/MainActivity.java
+++ helloworld/app/src/main/java/com/training/interns/mza/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.training.interns.mza.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
