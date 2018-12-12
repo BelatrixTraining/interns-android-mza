080b62720280b4961c3703f2325d9c59c73ed898
Evangelina Selman <evangelina.selman@fisglobal.com>
2018-12-12 18:37:26 -0300

    Adding Service Example

---
+++ Service/app/src/main/java/com/training/interns/mza/service/constants/ImageDownloaderConstants.java
@@ -0,0 +1,7 @@
+package com.training.interns.mza.service.constants;
+
+public class ImageDownloaderConstants {
+    public static final String IMAGE_PATH = "IMAGE_PATH";
+    public static final String IMAGE_URL = "IMAGE_URL";
+    public static final String INTENT_FILTER_DOWNLOAD_IMAGE = "download-image";
+}
