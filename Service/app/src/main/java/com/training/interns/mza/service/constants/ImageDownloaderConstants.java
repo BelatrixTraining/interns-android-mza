502970fa55c9e31529036d8d534961f3d2260b3a
eselman <evaselman@gmail.com>
2018-12-12 23:34:50 -0300

    Deleting Service

--- Service/app/src/main/java/com/training/interns/mza/service/constants/ImageDownloaderConstants.java
+++
@@ -1,7 +0,0 @@
-package com.training.interns.mza.service.constants;
-
-public class ImageDownloaderConstants {
-    public static final String IMAGE_PATH = "IMAGE_PATH";
-    public static final String IMAGE_URL = "IMAGE_URL";
-    public static final String INTENT_FILTER_DOWNLOAD_IMAGE = "download-image";
-}
